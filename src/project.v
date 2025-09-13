f585bba942df99fd93b3915286285f9e11667f71
denizozenli <denizozenli88@gmail.com>
2025-09-13 14:08:02 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_counter(;
+module tt_um_counter(
     input  wire [3:0] ui_in,    // Dedicated inputs
     output wire [3:0] uo_out,   // Dedicated outputs
     input  wire [3:0] uio_in,   // IOs: Input path
